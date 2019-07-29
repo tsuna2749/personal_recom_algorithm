b9e27da674bbe6ae64749afa1e9d4337f9305565
baoxiang <38351859+tsuna2749@users.noreply.github.com>
2019-07-29 14:04:29 +0800

    LFM(Latent factor model) algorithm
    Using LFM to build a simple recommender system based on items and users

---
+++ LFM/production/lfm.py
@@ -0,0 +1,118 @@
+#-*-coding:utf8-*-
+'''
+author:baoxiang
+date:2019.7.26
+lfm model train main function
+'''
+
+import numpy as np
+import sys
+sys.path.append("../util")
+import LFM.util.read as read
+import operator
+
+def lfm_train(train_data, F, alpha, beta, step):
+    '''
+    Args:
+        train_data: train data for lfm
+        F: user vector len, item vector len
+        alpha: regularization factor
+        beta: learning rate
+        step: iteration number
+    Return:
+        dict: key itemid, value np.ndarray
+        dict: key userid, value np.ndarray
+    '''
+
+    user_vec = dict()
+    item_vec = dict()
+
+    for step_index in range(step):
+        for data_instance in train_data:
+            userid, itemid, label = data_instance
+            if userid not in user_vec:
+                user_vec[userid] = init_model(F)
+            if itemid not in item_vec:
+                item_vec[itemid] = init_model(F)
+        delta = label - model_predict(user_vec[userid], item_vec[itemid])
+        for index in range(F):
+            user_vec[userid][index] += beta * (delta * item_vec[itemid][index] - alpha * user_vec[userid][index])
+            item_vec[itemid][index] += beta * (delta * user_vec[userid][index] - alpha * item_vec[itemid][index])
+        beta = beta * 0.9
+    return user_vec, item_vec
+
+def init_model(vector_len):
+    '''
+    Args:
+        vector_len: the length of vector
+    Return:
+        a ndarray
+    '''
+    return np.random.randn(vector_len)
+
+
+def model_predict(user_vector, item_vector):
+    '''
+    Args:
+        user_vector: model produce user vector
+        item_vector: model produce item vector
+    Return;
+        a number
+    '''
+    return np.dot(user_vector, item_vector) / (np.linalg.norm(user_vector) * np.linalg.norm(item_vector))
+
+def model_train_process():
+    '''
+    test lfm model train
+    '''
+    train_data = read.get_train_data('../data/ratings.txt')
+    user_vec, item_vec = lfm_train(train_data, 50, 0.01, 0.1, 50)
+    #print(user_vec['1'])
+    #print(item_vec['2455'])
+    recom_list = give_recom_result(user_vec, item_vec, '24')
+    ana_recom_result(train_data, '24', recom_list)
+
+def give_recom_result(user_vec, item_vec, userid):
+    '''
+    Args:
+        user_vec: lfm model result
+        item_vec:lfm model result
+        userid:fix userid
+    Return:
+        a list:[(itemid, score), (itemid1, score1)]
+    '''
+    fix_num = 10
+    if userid not in user_vec:
+        return []
+    record = {}
+    recom_list = []
+    user_vector = user_vec[userid]
+    for itemid in item_vec:
+        item_vector = item_vec[itemid]
+        res = np.dot(user_vector, item_vector) / (np.linalg.norm(user_vector) * np.linalg.norm(item_vector))
+        record[itemid] = res
+    for zuhe in sorted(record.items(), key = operator.itemgetter(1), reverse = True)[:fix_num]:
+        itemid = zuhe[0]
+        score = round(zuhe[1], 3)
+        recom_list.append((itemid, score))
+    return recom_list
+
+def ana_recom_result(train_data, userid, recom_list):
+    """
+    debug recom result for userid
+    Args:
+        train_data: train data for lfm model
+        userid:fix userid
+        recom_list: recom result by lfm
+    """
+    item_info = read.get_item_info('../data/movies.txt')
+    for data_instance in train_data:
+        tmp_userid, itemid, label = data_instance
+        if tmp_userid == userid and label == 1:
+            print(item_info[itemid])
+    print("recom result")
+    for zuhe in recom_list:
+        print(item_info[zuhe[0]])
+
+if __name__ == '__main__':
+    model_train_process()
